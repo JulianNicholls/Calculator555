392ff8f3766d2df9aee1d49044e3b3f93afb8650
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-11 22:12:52 +0100

    Update tests for refactored calculator.

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -7,61 +7,59 @@ require './calculator'
 class Calc555Test < Minitest::Test
   def setup
     @calc = Calculator555.new(22)
-    @calc.r1 = 402
-    @calc.r2 = 6400
+    @calc.r1_value = 402
+    @calc.r2_value = 6400
   end
 
   def test_raw_initialize
     calc = Calculator555.new(22) # Implied micro F
 
-    assert_equal 22.0 * 10**-6, calc.c
+    assert_equal 22.0 * 10**-6, calc.cap_value
   end
 
   def test_raw_initialize_with_string
     calc = Calculator555.new('22') # Implied micro F
 
-    assert_equal 22.0 * 10**-6, calc.c
+    assert_equal 22.0 * 10**-6, calc.cap_value
   end
 
   def test_uf_convert
     calc = Calculator555.new(47, 'uF')
 
-    assert_equal 47.0 * 10**-6, calc.c
+    assert_equal 47.0 * 10**-6, calc.cap_value
   end
 
   def test_mu_f_convert
     calc = Calculator555.new(47, 'µF')
 
-    assert_equal 47.0 * 10**-6, calc.c
+    assert_equal 47.0 * 10**-6, calc.cap_value
   end
 
   def test_nf_convert
     calc = Calculator555.new(47, 'nF')
 
-    assert_equal 47.0 * 10**-9, calc.c
+    assert_equal 47.0 * 10**-9, calc.cap_value
   end
 
   def test_pf_convert
     calc = Calculator555.new(47, 'pF')
 
-    assert_equal 47.0 * 10**-12, calc.c
+    assert_equal 47.0 * 10**-12, calc.cap_value
   end
 
   def test_ra
-    assert_equal 402, @calc.ra
-    assert_equal @calc.r1, @calc.ra
+    assert_equal 402, @calc.ra_value
   end
 
   def test_rb
-    assert_equal 6400, @calc.rb
-    assert_equal @calc.r2, @calc.rb
+    assert_equal 6400, @calc.rb_value
   end
 
-  def test_th_with_explicit_r1_r2
+  def test_th_with_explicit_resistors
     assert_in_delta 0.104, @calc.th, 0.0005
   end
 
-  def test_tl_with_explicit_r1_r2
+  def test_tl_with_explicit_resistors
     assert_in_delta 0.098, @calc.tl, 0.0005
   end
 
@@ -73,7 +71,7 @@ class Calc555Test < Minitest::Test
     assert_in_delta 98, @calc.tl_ms, 0.5
   end
 
-  def test_period_with_explicit_r1_r2
+  def test_period_with_explicit_resistors
     assert_in_delta 0.202, @calc.period, 0.0005
   end
 
@@ -81,13 +79,13 @@ class Calc555Test < Minitest::Test
     assert_in_delta 202, @calc.period_ms, 0.5
   end
 
-  def test_frequency_hz_Hz_with_explicit_r1_r2
+  def test_frequency_hzs_with_explicit_resistors
     assert_in_delta 4.95, @calc.hz, 0.0005
     assert_in_delta 4.95, @calc.Hz, 0.0005
     assert_in_delta 4.95, @calc.frequency, 0.0005
   end
 
-  def test_duty_ratio_with_explicit_r1_r2
+  def test_duty_ratio_with_explicit_resistors
     assert_in_delta 0.515, @calc.duty_ratio, 0.0005
   end
 
@@ -95,43 +93,43 @@ class Calc555Test < Minitest::Test
     assert_in_delta 51.5, @calc.duty_ratio_percent, 0.05
   end
 
-  def test_calculate_r1_r2_duty_ratio_period
+  def test_calculate_resistors_duty_ratio_period
     @calc.duty_ratio = 0.55   # 55%
     @calc.period = 0.200      # 200ms
 
-    assert_in_delta 5903, @calc.r2, 1
-    assert_in_delta 1312, @calc.r1, 1
+    assert_in_delta 5903, @calc.r2_value, 1
+    assert_in_delta 1312, @calc.r1_value, 1
   end
 
-  def test_calculate_r1_r2_duty_ratio_percent_period
+  def test_calculate_resistors_duty_ratio_percent_period
     @calc.duty_ratio = 55   # 55%
     @calc.period = 0.200    # 200ms
 
-    assert_in_delta 5903, @calc.r2, 1
-    assert_in_delta 1312, @calc.r1, 1
+    assert_in_delta 5903, @calc.r2_value, 1
+    assert_in_delta 1312, @calc.r1_value, 1
   end
 
-  def test_calculate_r1_r2_duty_ratio_percent_period_ms
+  def test_calculate_resistors_duty_ratio_percent_period_ms
     @calc.duty_ratio = 55  # 55%
     @calc.period = 200     # 200ms
 
-    assert_in_delta 5903, @calc.r2, 1
-    assert_in_delta 1312, @calc.r1, 1
+    assert_in_delta 5903, @calc.r2_value, 1
+    assert_in_delta 1312, @calc.r1_value, 1
   end
 
-  def test_calculate_r1_r2_duty_ratio_percent_frequency
+  def test_calculate_resistors_duty_ratio_percent_frequency
     @calc.duty_ratio = 55  # 55%
     @calc.frequency = 5    # 5 Hz
 
-    assert_in_delta 5903, @calc.r2, 1
-    assert_in_delta 1312, @calc.r1, 1
+    assert_in_delta 5903, @calc.r2_value, 1
+    assert_in_delta 1312, @calc.r1_value, 1
   end
 
-  def test_calculate_r1_r2_duty_ratio_percent_hz
+  def test_calculate_resistors_duty_ratio_percent_hz
     @calc.duty_ratio = 55   # 55%
     @calc.hz = 5            # 5 Hz
 
-    assert_in_delta 5903, @calc.r2, 1
-    assert_in_delta 1312, @calc.r1, 1
+    assert_in_delta 5903, @calc.r2_value, 1
+    assert_in_delta 1312, @calc.r1_value, 1
   end
 end
