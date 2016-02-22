5ab0cfb733e6fab11c10b03d9802e2a0514cb856
Julian Nicholls <juliannicholls29@gmail.com>
2016-02-22 21:07:15 +0000

    Update display for small resistor values.

--- rformat.rb
+++ rformat.rb
@@ -1,11 +1,14 @@
 # Resistor value formatter
 #
-# 0-5K    Display in Ohms
-# %k-1M   Display in kOhms to 2DP
+# 0-10    Display IN Ohms to 3DP with a warning
+# 1-5K    Display in Ohms
+# 5K-1M   Display in kOhms to 2DP
 # 1M+     Display in MOhhms to 2DP
 class ResistorFormatter
   def self.str(value)
-    if value < 5_000.0
+    if value < 10.0
+      value.round(3).to_s +  ' Ω - Warning Low Value'
+    elsif value < 5_000.0
       value.round.to_s + ' Ω'
     elsif value < 1_000_000.0
       (value / 1_000.0).round(2).to_s + ' kΩ'
